376cf88f644810b7f8441925d7bd685408801f74
Jhonny Mezzari <jhonnybif@gmail.com>
2023-05-24 19:25:47 -0300

    selectByOrdem

--- src/database/dao/ProcessoDAO.java
+++ src/database/dao/ProcessoDAO.java
@@ -11,10 +11,14 @@ import database.model.Processo;
 public class ProcessoDAO {
 	
 	private String select = "select * from tb_processo where habilitado";
+	private String selectByOrdem = "select * from tb_processo_tabela where id_processo = ? and habilitado order by ordem";
+	
 	private PreparedStatement pstSelect;
+	private PreparedStatement pstSelectByOrdem;
 	
 	public ProcessoDAO(Connection conn) throws SQLException {
 		pstSelect = conn.prepareStatement(select);
+		pstSelectByOrdem = conn.prepareStatement(selectByOrdem);
 	}
 	
 	public ArrayList<Processo> selectAll() throws SQLException {
@@ -22,6 +26,24 @@ public class ProcessoDAO {
 		ArrayList<Processo> arlProcessos = new ArrayList<Processo>();
 		
 		ResultSet resultado = pstSelect.executeQuery();
+		while (resultado.next()) {
+			Processo p = new Processo();
+			p.setId(resultado.getInt("id"));
+			p.setNome_processo(resultado.getString("nome_processo"));
+			p.setDescricao(resultado.getString("descricao"));
+			p.setHabilitado(resultado.getBoolean("habilitado"));
+			arlProcessos.add(p);
+		}
+		
+		return arlProcessos;
+		
+	}
+	
+	public ArrayList<Processo> selectByOrdem() throws SQLException {
+		
+		ArrayList<Processo> arlProcessos = new ArrayList<Processo>();
+		
+		ResultSet resultado = pstSelectByOrdem.executeQuery();
 		while (resultado.next()) {
 			Processo p = new Processo();
 			p.setId(resultado.getInt("id"));
